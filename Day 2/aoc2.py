67a113611cdcbbcbfe6075cefc25b6958b060807
lucian606 <iliesculucian1508@gmail.com>
2020-12-02 14:22:03 +0200

    Add day 1 and day 2

---
+++ Day 2/aoc2.py
@@ -0,0 +1,33 @@
+def xor(a, b):
+    return a ^ b
+
+input_file = open('input.txt','r')
+lines = input_file.readlines()
+valid_passwords = 0
+new_valid_passwords = 0
+
+for line in lines:
+    args = line.split(" ")
+    args[0] = args[0].split("-")
+    lower_bound = int(args[0][0])
+    upper_bound = int(args[0][1])
+    chr = args[1][0]
+    password = args[2]
+    occurences = password.count(chr)
+    if occurences >= lower_bound and occurences <= upper_bound:
+        valid_passwords += 1
+
+print(valid_passwords)
+
+for line in lines:
+    args = line.split(" ")
+    args[0] = args[0].split("-")
+    first_index = int(args[0][0])
+    second_index = int(args[0][1])
+    chr = args[1][0]
+    password = args[2]
+    password = " " + password
+    if xor(password[first_index] == chr, password[second_index] == chr):
+        new_valid_passwords += 1
+
+print(new_valid_passwords)
