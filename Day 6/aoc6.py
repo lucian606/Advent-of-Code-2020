ec5e4d8771d20e3b373f7fcf2d93f4d387b7f149
lucian606 <iliesculucian1508@gmail.com>
2020-12-06 23:20:33 +0200

    Add day 6

---
+++ Day 6/aoc6.py
@@ -0,0 +1,39 @@
+input_file = open('input.txt', 'r')
+lines = input_file.readlines()
+total_answers = 0
+unanimous_answers = 0
+groups = []
+current_group = ""
+
+for line in lines:
+    if line != '\n':
+        current_group += line
+    else:
+        groups.append(current_group)
+        current_group = ""
+        
+groups.append(current_group)
+
+for group in groups:
+    answered = [False] * 27
+    for answer in group:
+        if answer != '\n':
+            answered[ord(answer) - ord('a')] = True
+    total_answers += len(list(filter(lambda x: x, answered)))
+
+print(total_answers)
+
+for group in groups:
+    answered = [False] * 27
+    members = group.count('\n')
+    if group == groups[-1]:
+        members += 1
+    for answer in group:
+        if answer != '\n':
+            if group.count(answer) == members:
+                answered[ord(answer) - ord('a')] = True
+            else:
+                answered[ord(answer) - ord('a')] = False
+    unanimous_answers += len(list(filter(lambda x: x, answered)))
+
+print(unanimous_answers)
