8a663b771f4931b9a2462dcd4aa8df9a6d51853b
lucian606 <iliesculucian1508@gmail.com>
2020-12-11 01:47:01 +0200

    solve p2 of day 10

--- Day 10/aoc10.py
+++ Day 10/aoc10.py
@@ -1,9 +1,19 @@
+def numberOfArrangements(arr):
+    DP = [0] * len(arr)
+    DP[0] = 1
+    for i in range(0, len(arr)):
+        for k in range(i + 1, len(DP)):
+            if arr[k] - arr[i] <= 3:
+                DP[k] += DP[i]
+    return DP[-1]
+
 joltages = []
 with open('input.txt') as input_file:
     for line in input_file:
         joltage = int(line)
         joltages.append(joltage)
 joltages.sort()
+joltages.insert(0, 0)
 joltages.append(joltages[-1] + 3)
 one_jolt_diff = 0
 three_jolt_diff = 0
@@ -15,3 +25,4 @@ for joltage in joltages:
         one_jolt_diff += 1
     current_joltage = joltage
 print(f"Part one: {one_jolt_diff * three_jolt_diff}")
+print(f"Part two: {numberOfArrangements(joltages)}")
